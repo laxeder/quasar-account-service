7309271a54c8d210f4b6c4f6b582fc0b7b7753b0
Laxeder <laxederbr@gmail.com>
2024-02-04 09:57:29 -0300

    feat(header): Adicionando cabeçalho

---
+++ src/components/MHeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-header class="bg-primary" elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="$emit('toggle-left-drawer')"
+      />
+
+      <q-toolbar-title>Serviço de contas</q-toolbar-title>
+
+      <q-btn color="secondary" text-color="primary" @click="onLogin"
+        >Entrar</q-btn
+      >
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+defineEmits(['toggle-left-drawer']);
+
+const router = useRouter();
+
+function onLogin() {
+  router.push('/login');
+}
+</script>
