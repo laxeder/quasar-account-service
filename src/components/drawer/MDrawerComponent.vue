01fac2115205eeb1a7847055e05c776163ba0e35
Laxeder <laxederbr@gmail.com>
2024-02-04 09:57:00 -0300

    feat(drawer): Adicionando gaveta

---
+++ src/components/drawer/MDrawerComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-list>
+      <q-item-label header>Menu da aplicação</q-item-label>
+
+      <MDrawerItemComponent
+        v-for="item in drawerItems"
+        :key="item.title"
+        v-bind="item"
+      />
+    </q-list>
+  </q-drawer>
+</template>
+
+<script setup lang="ts">
+import MDrawerItemComponent, {
+  DrawerItemProps,
+} from './MDrawerItemComponent.vue';
+
+const leftDrawerOpen = defineModel('leftDrawerOpen', { type: Boolean });
+
+const drawerItems: DrawerItemProps[] = [
+  {
+    title: 'Documentação',
+    caption: 'README.md',
+    icon: 'school',
+    link: 'https://github.com/laxeder/quasar-account-service/grob/main/README.md',
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/laxeder/quasar-account-service',
+    icon: 'code',
+    link: 'https://github.com/laxeder/quasar-account-service',
+  },
+  {
+    title: 'Criador',
+    caption: 'github.com/laxeder',
+    icon: 'person',
+    link: 'https://github.com/laxeder',
+  },
+];
+</script>
